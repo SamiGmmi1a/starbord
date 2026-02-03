7ca100b528b172edc15aa035ddfddf5003a5dee4
SamiGmmi1a <sghorzi.univ.fcomte@gmail.com>
2026-02-03 22:22:34 +0100

    amélioration fonction connexion

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,9 +1,16 @@
 // auth.js — gestion simple utilisateur
 
 const Auth = {
-  login(userId, token) {
-    localStorage.setItem("userId", userId);
-    localStorage.setItem("authToken", token);
+  login(email, code) {
+    const allowedEmail = VALID_CODES[code];
+
+    if (allowedEmail && allowedEmail === email) {
+      const token = "token_" + Math.random().toString(36).substring(2);
+      localStorage.setItem("userId", email);
+      localStorage.setItem("authToken", token);
+      return true;
+    }
+    return false;
   },
 
   logout() {
@@ -13,9 +20,17 @@ const Auth = {
 
   isLoggedIn() {
     return !!localStorage.getItem("authToken");
-  },
-
-  getUserId() {
-    return localStorage.getItem("userId");
   }
 };
+
+
+const VALID_CODES = {
+  "STB#1001": "client1@mail.com",
+  "STAR#1002": "client2@mail.com",
+  "STAR#1003": "client3@mail.com"
+};
+
+
+
+
+
